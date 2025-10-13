c5edbd5a2fe0e34f9fb9ec70360dbff391f23373
Rahat_Islam <rahatbottt@gmail.com>
2025-10-13 21:30:10 +0600

    Update Bot.js

--- Script/commands/Bot.js
+++ Script/commands/Bot.js
@@ -99,8 +99,8 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস উল্লাস এর বউ এর নাম..!😠🥰⛏️", threadID);
    };
   
-  if ((event.body.toLowerCase() == "mim") || (event.body.toLowerCase() == "Mim")) {
-     return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস উল্লাস এর বউ এর নাম..!😠🥰⛏️", threadID);
+  if ((event.body.toLowerCase() == "@Riya Moni") || (event.body.toLowerCase() == "Mim")) {
+     return api.sendMessage("খবরদার কেউ ম্যানশন দিবানা এটা আমার বস রাহাদ এর gf হয়😾😠", threadID);
    };
 
   if ((event.body.toLowerCase() == "Arohi") || (event.body.toLowerCase() == "farhana")) {
