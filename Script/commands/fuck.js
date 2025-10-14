e814805e8381ff06be9d5b94388804dcec67cf61
Rahat_Islam <rahatbottt@gmail.com>
2025-10-14 12:37:02 +0600

    Update fuck.js

--- Script/commands/fuck.js
+++ Script/commands/fuck.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 2,
     credits: "C B T",
     description: "Get fuck",
-    commandCategory: "nsfw",
+    commandCategory: "চুদে_দিবো",
     usages: "[@mention]",
     cooldowns: 5,
     dependencies: {
